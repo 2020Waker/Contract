d817bb7a711228c583699111707caf0d44ac0a34
2020Waker <893601350@qq.com>
2024-10-24 23:00:29 +0800

    111

--- contracts/HelloComrades.sol
+++ contracts/HelloComrades.sol
@@ -7,18 +7,63 @@ contract HelloComrades {
      * State Variables
      * =========================
      */
-
+    /*
+    0:等待领导说“同志们好”
+    1:等待同志们回复“领导好”
+    2:等待领导说“同志们辛苦了”
+    3:等待同志们回复“为人民服务”
+    4:等待销毁合约
+    */
+    uint8 public step = 0;
     address public immutable leader;
-
+    string internal constant UNKNOWN =
+        unicode"我不知道如何处理它，你找有关部门吧！";
     /*
      * =========================
      * Events
      * =========================
      */
+
+    event Step(uint8);
     event Log(string tag, address from, uint256 amount, bytes data);
 
-    constructor() {
-        
+    /// 这里的错误信息会被打印出来
+    error MyError(string msg);
+
+    constructor(address leader_) {
+        require(leader_ != address(0), "invalid address!");
+        leader = leader_;
+    }
+
+    /*
+     * =========================
+     * Modifier
+     * =========================
+     */
+    modifier onlyLeader() {
+        require(msg.sender == leader, unicode"必须领导才能说");
+        _;
+    }
+    modifier onlySubordinate() {
+        require(msg.sender != leader, unicode"下属才能说");
+        _;
+    }
+
+    /*
+     * =========================
+     * Fucntions 函数
+     * =========================
+     */
+    function hello(string calldata content) external onlyLeader returns (bool) {
+        if (step != 0) {
+            revert(UNKNOWN);
+        }
+        if (!review(content,unicode"同志们好")) {
+            revert MyError(unicode"必须说：同志们好！");
+        }
+        step = 1;
+        emit Step(step);
+        return true;
     }
 
     /*
@@ -26,13 +71,8 @@ contract HelloComrades {
      * Helper 辅助函数
      * =========================
      */
-    /*
-     * =========================
-     * Helper 辅助函数
-     * =========================
-     */
 
-    function review2(string calldata content, string calldata correctContent)
+    function review(string calldata content, string memory correctContent)
         private
         pure
         returns (bool)
